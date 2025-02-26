5846ff0e616dcd7abd045a9682d3b10be5b96cce
Martin Rodriguez <martinrodriguez493@hotmail.com>
2025-02-26 00:17:27 -0300

    se adapto el template, se modifico el menu, se agrego el logo, se edito el  footer, etc

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,47 @@
+ <!-- Page Preloder -->
+ <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="container">
+            <div class="row ">
+                <div class="col-lg-2 col-md-2">
+                    <div class="header__logo">
+                        <a href="/">
+                            <img src="{{asset('img/MARCA_BLANCO_PNG.png')}}" alt="Logo Grupo Irundy">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-10 col-md-10">
+                    <div class="header__nav">
+                        <nav class="header__menu mobile-menu">
+                            <ul>
+                                <li class="active"><a href="/">Inicio</a></li>                                
+                                <li><a href="/">Biografia</a></li>
+                                <li><a href="/">Fotos</a></li>
+                                <li><a href="/">Videos</a></li>
+                                <!-- <li><a href="#">Pages</a>
+                                    <ul class="dropdown">
+                                        <li><a href="./about.html">About</a></li>
+                                        <li><a href="./blog.html">Blog</a></li>
+                                        <li><a href="./blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li> -->
+                                <li><a href="/">Contactos</a></li>
+                            </ul>
+                        </nav>
+                        <div class="header__right__social">
+                            <a href="https://www.facebook.com/share/1P1QAv2iCy/?mibextid=wwXIfr"><i class="fa fa-facebook"></i></a>
+                            <!-- <a href="#"><i class="fa fa-twitter"></i></a> -->
+                            <a href="https://www.instagram.com/grupo_irundy"><i class="fa fa-instagram"></i></a>
+                            <a href="https://www.youtube.com/@grupoirundy"><i class="fa fa-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="mobile-menu-wrap"></div>
+        </div>
+    </header>
+    <!-- Header Section End -->
