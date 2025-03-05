5cd33174a49b0494bd5af0ff837394aaf5d5f72e
Sscheinberg <agr.scheinberg@gmail.com>
2025-03-05 07:35:04 -0300

    formulario login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login() {
+        return view("auth.login");
+    }
+    public function auth(Request $request) {
+
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('admin/dashboard');
+        }
+
+        return back()->withErrors(['email' => 'Credenciales incorrectas']);
+    }
+
+
+    public function showRegister()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->route('login')->with('success', 'Registro exitoso. Inicia sesión.');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+
+Route::get('/login', [LoginController::class, 'login']);
+
+Route::post('/login', [LoginController::class, 'auth'])->name('login');
+
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware('auth')->name('dashboard');
